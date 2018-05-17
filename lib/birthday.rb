4834afc612484bd8a65d4b926500efe0e1e84354
taryn yaeger <tsyaeger@gmail.com>
2018-05-17 06:39:12 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,7 @@ birthday_kids = {
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name,age|
   
-  "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  "Happy Birthday #{name}! You are now #{age} years old!"
 end
 
 
