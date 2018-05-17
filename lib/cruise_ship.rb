18d99cdc2b917a4d70f3753651eee511d5714172
taryn yaeger <tsyaeger@gmail.com>
2018-05-17 06:59:24 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,15 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+passengers = {
+suite_a: "Amanda Presley", 
+suite_b: "Seymour Hoffman", 
+suite_c: "Alfred Tennyson", 
+suite_d: "Charlie Chaplin", 
+suite_e: "Crumpet the Elf"
+}
+
 
 def select_winner(passengers)
-  # add the code snippet here!
+  pass = passengers[:suite_a]
+  puts pass
+  return pass if pass[0].upcase == 'A'
+
 end
